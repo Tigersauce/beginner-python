322f7ba34a6daaece13346f5c63f79c0fb761edf
Tigersauce <jaydison@yahoo.com>
2021-10-29 18:29:33 -0400

    Almost done with section one

--- Hello.py
+++ Hello.py
@@ -1,1 +1,56 @@
 # Don't delete me
+print(5+5, "hey")
+age = 26
+print(age+10)
+val = 10/5+3*3
+print(val)
+val + ((10/5) + (3*3))
+print (val)
+"""This is a knockoff
+multiline comment
+"""
+# this
+# also
+# works
+
+msg=("Comment")
+print(msg)
+
+
+# legal variable names
+age = 26 #give the variable that means something, age = someone's age, etc.
+age1 = 1 #yes
+age_2 = 2 #underscores are good
+#age-3 = 3 not good, don't use "-"
+#4age=4 don't start with a number
+Age = 6 #works --> not good though. lowercase for variables
+age_of_user = 7 #this also works
+#return = 5 return is a keyword, not a variable
+
+# operators
+import math
+
+result= 10//3
+
+print(result)
+
+# / --> float division
+# // --> integer division (math.floor)
+
+#modulus operator
+#gives the remainder of some division
+10/3
+pizza = 10
+people = 3
+print(10%3) #or
+print("leftover", pizza % people)
+
+number = 345345
+limit = 10
+print(number % limit)
+
+#raising numbers to a power
+import math
+result = math.pow(3, 5)
+print(3 ** 3)
+print(result)
